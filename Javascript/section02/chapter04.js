b8ae4730bbc1259093d42d893e0e0bb455ea50d0
minhuits <a41417144@gmail.com>
2025-04-08 13:17:02 +0900

    섹션02. Javasript 기본

---
+++ Javascript/section02/chapter04.js
@@ -0,0 +1,24 @@
+// 1. 변수
+let age = 10;
+
+age = 30;
+
+// 2. 상수
+const pi = 3.14;
+
+// pi = 10; // 에러 발생
+
+/*
+3. 변수 명명규칙(네이밍 규칙)
+3-1. $, _ 제외한 기호는 사용할 수 없다.
+3-2. 예약어를 사용할 수 없다.
+*/
+
+let $name;
+let name1;
+let $2name;
+
+// 4. 변수 명명 가이드
+let salesCount = 1;
+let refundCount = 2;
+let tatalSalesCount = salesCount - refundCount;
